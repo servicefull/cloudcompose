17ff4adbc67871685d1546bdf7453a55c2d0bf32
servicefull <rob.bowers@gmail.com>
2017-09-04 23:30:25 -0400

    rm build

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 'use strict';
 
 const up = require('./services/up');
-const build = require('./services/build');
 
 const _up = (options,callback) => {
 	return up(options,callback);
 }
 
-const _build = (options,callback) => {
-	return build(options,callback);
-}
-
 module.exports = {
-	up:_up,
-	build:_build
+	up:_up
 }
